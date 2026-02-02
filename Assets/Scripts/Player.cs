9b71667d666f0194705f6d251ebf894ed181c0c2
PotapenkoVadim <potapenko.v00@mail.ru>
2026-02-02 20:12:32 +0300

    feat: collision

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
   [SerializeField] private GameInput _gameInput;
 
   private bool _isWalking = false;
+  private readonly float _playerRadius = .7f;
+  private readonly float _playerHeight = 2f;
 
   private void Update()
   {
@@ -20,8 +22,56 @@ public class Player : MonoBehaviour
 
   private void Walk(Vector3 moveDirection)
   {
-    transform.position += _moveSpeed * Time.deltaTime * moveDirection;
-    _isWalking = moveDirection != Vector3.zero;
+    float moveDistance = _moveSpeed * Time.deltaTime;
+    bool canMove = !Physics.CapsuleCast(
+      transform.position,
+      transform.position + Vector3.up * _playerHeight,
+      _playerRadius,
+      moveDirection,
+      moveDistance
+    );
+
+    if (!canMove)
+    {
+      Vector3 moveDirX = new Vector3(moveDirection.x, 0, 0).normalized;
+
+      canMove = !Physics.CapsuleCast(
+        transform.position,
+        transform.position + Vector3.up * _playerHeight,
+        _playerRadius,
+        moveDirX,
+        moveDistance
+      );
+
+      if (canMove)
+      {
+        moveDirection = moveDirX;
+      } else
+      {
+        Vector3 moveDirZ = new Vector3(0, 0, moveDirection.z).normalized;
+        canMove = !Physics.CapsuleCast(
+          transform.position,
+          transform.position + Vector3.up * _playerHeight,
+          _playerRadius,
+          moveDirZ,
+          moveDistance
+        );
+
+        if (canMove)
+        {
+          moveDirection = moveDirZ;
+        } else
+        {
+          
+        }
+      }
+    }
+
+    if (canMove)
+    {
+      transform.position += moveDistance * moveDirection;
+      _isWalking = moveDirection != Vector3.zero;
+    }
   }
 
   private void Rotate(Vector3 moveDirection)
